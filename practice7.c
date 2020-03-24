946e8e624a000ff95a627fa41210621c9ee14118
Gentle-Hui <1120365945@qq.com>
2020-03-24 21:58:34 +0800

    upload

---
+++ practice7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+void Swap(int *pa, int *pb){
+	int tmp = 0;
+	tmp = *pa;//µÈ¼ÛÓÚtmp=a;
+	*pa = *pb;
+	*pb = tmp;
+}
+
+int main(){
+	int a = 10;
+	int b = 20;
+	printf("a=%d b=%d\n", a, b);
+	Swap(&a, &b);
+	printf("a=%d b=%d\n", a, b);
+	system("pause");
+	return 0;
+}
