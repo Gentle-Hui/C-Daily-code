9626dc5b0fced1ece1a0d79bd6f61b96a12b723f
Gentle-Hui <1120365945@qq.com>
2020-04-02 22:31:07 +0800

    upload

---
+++ 不用中间变量的变量交换.c
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+void exchange(int m, int n){
+	m = m^n;
+	n = m^n;
+	m = m^n;
+	printf("交换后得知为：\ni=%d和j=%d\n", m, n);
+}
+
+
+int main()
+{
+	int i = 0;
+	int j = 0;
+	printf("请输入i的值：\n");
+	scanf("%d", &i);
+	printf("请输入j的值：\n");
+	scanf("%d", &j);
+	printf("您输入的值为：\ni=%d和j=%d\n", i, j);
+	exchange(i, j);
+	system("pause");
+	return 0;
+}
