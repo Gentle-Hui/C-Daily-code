5d3a21f52ab695d7004ed899b56b6883425a2131
Gentle-Hui <1120365945@qq.com>
2020-04-03 21:26:43 +0800

    upload

---
+++ 指针打印数组.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void print(int *num, int length){
+	int i = 0;
+	int* p = num;
+	for (i = 0; i < length; i++){
+		printf("%d ",*(p+i));
+	}
+}
+
+
+int main(){
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr,sz);
+	system("pause");
+	return 0;
+}
